e175d4308e24cc4448b1eacebcc6034d4fc76cd3
Igor Fala <ifala88@gmail.com>
2016-01-18 20:48:42 -0500

    add more features to scraper

--- Web/Page-Scraper/README.md
+++ Web/Page-Scraper/README.md
@@ -1,4 +1,5 @@
-Project: Web Scraper for images and links.\n
+**Project: Web Scraper for images and links.**
+
          To use it, import [scraper](https://github.com/igorfala/karan-projects/blob/master/Web/Page-Scraper/scraper.py) and
          call the : linkScraper(URL,DB) function.
          or imageScraper(URL, DB), where URL is the Uniform Resource Locator and
--- Web/Page-Scraper/scraper.py
+++ Web/Page-Scraper/scraper.py
@@ -3,6 +3,9 @@ Project: Web Scraper for images and links
          To use it, import the module and
          call the : linkScraper(URL,DB) function.
          or imageScraper(URL, DB)
+         To download all the images saved in the DB, 
+         simply call imageDownload(DB), only after 
+         imageScraper() was called
          
 @author: Igor Fala
 
@@ -11,7 +14,7 @@ date: 01/12/2016
 
 from urllib import urlopen
 from bs4 import BeautifulSoup
-import sqlite3
+import sqlite3, os
 
 def linkScraper(URL, DB):
     
@@ -65,4 +68,47 @@ def imageScraper(URL, DB):
         conn.commit()
     cur.close()
     
-if __name__ = '__main__': print __doc__
+def download(src, fileName): # src is the image link, fileName is the name of
+    fsock = urlopen(src)     # the file it will be saved under
+    fObj = open(fileName, 'wb')
+    fObj.write(fsock.read())
+    fsock.close()
+    fObj.close()
+
+def imageDownload(DB):
+    conn = sqlite3.connect(DB)
+    cur = conn.cursor()
+    try:              # Verify if the DB with src was created
+        sources = cur.execute('''SELECT source FROM Images''')
+    except sqlite3.OperationalError:
+        print "Please call imageScraper() first, to create a database with image sources."
+        return
+    count = 0
+    if not os.path.exists('IMAGES'):      # Create the 'Images' directory if it 
+        os.makedirs('IMAGES')             # does not exist.
+    os.chdir(os.getcwd()+'/IMAGES')       # Change to 'Images' to save files there.
+
+    if os.listdir(os.getcwd()) != []:
+        print "Directory not empty, removing all contents before proceding"
+        inp = raw_input('To continue pres "Y", to abort press "N":\t')
+        if inp in ("Y", 'y'):
+            print "Deleting all files, to place new images."
+            for f in os.listdir(os.getcwd()):
+                os.remove(f)
+        else:
+            print "Aborted"
+            os.chdir(os.path.dirname(os.getcwd())) # Changing back to parent directory
+            return
+    print 'Downloading...'
+    for source in sources:
+        src = source[0]                   # Get the actual text (source is a tuple).
+        try:
+            name = src.split('/')[-1]
+            download(src, name)
+        except:
+            print " Couldn't save: %s" % src
+            pass
+    print 'Finished, check IMAGES'
+    os.chdir(os.path.dirname(os.getcwd())) # Changing back to parent directory
+
+if __name__ == '__main__': print __doc__
